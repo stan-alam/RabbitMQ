17779a0d382b37532a70ff410a5c84645a945155
Stan Alam <stan.alam@gmail.com>
2018-02-08 17:00:50 -0500

    created outline for client app

---
+++ src/client.py
@@ -0,0 +1,20 @@
+/*
+
+technical implementation tasks required by the NYSE street publisher to complete
+
+1. Connect to the RabbitMQ server
+
+2. Obtain a channel
+
+3. Declare a queue
+
+4. Bind the queue with the exchange
+
+5. Consume the messages
+
+6. Close the channel
+
+7. Close the connection
+
+
+*/
